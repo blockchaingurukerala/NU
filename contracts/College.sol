694f400a5b9b7603be1c33965de5e40b3069f826
blockchaingurukerala <blockchaingurukerala@gmail.com>
2022-05-13 18:46:07 +0530

    admin dashboard

--- contracts/College.sol
+++ contracts/College.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.8.0;
 contract College{
     uint public totalUsers=0;
+    address public admin;
     mapping(address=>string) public roles;
     struct UserInfo{
         string name;
@@ -11,12 +12,14 @@ contract College{
     }
     mapping(uint=>UserInfo) public users;
     mapping(address=>UserInfo) public getUserInfo;
+    constructor() public {
+        admin=msg.sender;
+    }
     function addUser(string memory _n,string memory _e,string memory _p,string memory _r) public {
         totalUsers++;
         roles[msg.sender]=_r;
         users[totalUsers]=UserInfo(_n,_e,_p,_r);
         getUserInfo[msg.sender]=UserInfo(_n,_e,_p,_r);
-
     }
 }
 
